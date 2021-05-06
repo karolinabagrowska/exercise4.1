9631d6034ad1b329957872df7e81944639f366dc
Karolina Bagrowska <karolina.bagrowska@gmail.com>
2021-05-06 20:51:56 +0200

    4.2. Add endpoint/products/id

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sqlite3
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi import Response, status
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
 
+class Product(BaseModel):
+    id = 0
+    name = str
 
 @app.on_event("startup")
 async def startup():
@@ -59,4 +64,16 @@ async def customers(response: Response):
         "customers": customer_array
     }
 
-
+@app.get("/products/{id}")
+async def products(response: Response, id: int):
+    app.db_connection.row_factory = sqlite3.Row
+    product = app.db_connection.execute("SELECT ProductID, ProductName FROM Products WHERE ProductID = :id", {'id': id}).fetchone()
+    if product is None:
+        raise HTTPException(status_code=404)
+    else:
+        response.status_code = status.HTTP_200_OK
+    new_dict = {
+        "id": product["ProductID"],
+        "name": product["ProductName"]
+    }
+    return new_dict
